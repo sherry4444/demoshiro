8b9dde0ccc03c076e12971ea7b1e974d026d3ccf
LAPTOP-J3101MEF\74788 <597356241@qq.com>
2017-07-08 17:02:01 +0800

    add  a simple login-autentication with shiro

---
+++ src/main/java/com/example/demo/LoginController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+/**
+ * Created by 74788 on 2017/7/8.
+ */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login.html")
+    public String loginTemplate() {
+
+        return "login";
+    }
+
+
+    @RequestMapping("/index")
+    public String indexTemplate() {
+
+        return "index";
+    }
+
+}
